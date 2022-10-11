d166743a727d07f9f8d0c0429252ac889225de34
Said716 <115469947+Said716@users.noreply.github.com>
2022-10-11 14:21:58 +0300

    test

--- main.py
+++ main.py
@@ -1,76 +1,76 @@
-import urllib.request
-from urllib.parse import urlsplit, urlunsplit, urljoin, urlparse
-import re
-from multiprocessing import Process
-
-
-class urlparser:
-    def __init__(self,url,exclude = None,no_verbose = False  ):
-        self.url = self.normalize(url)
-        self.host = urlparse(self.url).netloc
-        self.exclude = exclude
-        self.no_verbose = no_verbose
-        self.visited_links = [self.url]
-        self.found_links = []
-
-
-    def normalize(self,url):
-        scheme, netloc, path, qs, anchor = urlsplit(url)
-        return urlunsplit((scheme, netloc, path, qs, anchor))
-
-
-    def is_internal(self,url):
-        host = urlparse(url).netloc
-        return host == self.host or host == ''
-
-
-    def is_url(self,url):
-        scheme, netloc, path, qs, anchor = urlsplit(url)
-        if url != '' and scheme in ['http', 'https', '']:
-            return True
-        else:
-            False
-
-    def find(self,url):
-        print(url)
-        try:
-            response = urllib.request.urlopen(url)
-
-        except:
-            print('404 Error' )
-            return
-        page = str(response.read())
-        pattern = '<a [^>]*href=[\'|"](.*?)[\'"].*?>'
-        found_links = re.findall(pattern,page )
-        links = []
-        for link in found_links:
-            is_url = self.is_url(link)
-            if is_url:
-                is_internal = self.is_internal(url)
-                if is_internal:
-                    self.add(link, links, self.exclude)
-                    self.add(link, self.found_links, self.exclude)
-        for link in links:
-            if link not in self.visited_links:
-                link = self.normalize(link)
-                self.visited_links.append(link)
-                self.find(urljoin(self.url,link))
-
-
-    def start(self):
-        self.find(self.url)
-        return self.found_links
-
-
-    def add(self, link,link_list,eclude_pattern = None ):
-        link = self.normalize(link)
-        if link:
-            notinlist = link not in link_list
-            exclude = False
-            if eclude_pattern :
-                exclude = re.search(eclude_pattern,link)
-            if notinlist and not exclude:
-                link_list.append(link)
+# import urllib.request
+# from urllib.parse import urlsplit, urlunsplit, urljoin, urlparse
+# import re
+# from multiprocessing import Process
+#
+#
+# class urlparser:
+#     def __init__(self,url,exclude = None,no_verbose = False  ):
+#         self.url = self.normalize(url)
+#         self.host = urlparse(self.url).netloc
+#         self.exclude = exclude
+#         self.no_verbose = no_verbose
+#         self.visited_links = [self.url]
+#         self.found_links = []
+#
+#
+#     def normalize(self,url):
+#         scheme, netloc, path, qs, anchor = urlsplit(url)
+#         return urlunsplit((scheme, netloc, path, qs, anchor))
+#
+#
+#     def is_internal(self,url):
+#         host = urlparse(url).netloc
+#         return host == self.host or host == ''
+#
+#
+#     def is_url(self,url):
+#         scheme, netloc, path, qs, anchor = urlsplit(url)
+#         if url != '' and scheme in ['http', 'https', '']:
+#             return True
+#         else:
+#             False
+#
+#     def find(self,url):
+#         print(url)
+#         try:
+#             response = urllib.request.urlopen(url)
+#
+#         except:
+#             print('404 Error' )
+#             return
+#         page = str(response.read())
+#         pattern = '<a [^>]*href=[\'|"](.*?)[\'"].*?>'
+#         found_links = re.findall(pattern,page )
+#         links = []
+#         for link in found_links:
+#             is_url = self.is_url(link)
+#             if is_url:
+#                 is_internal = self.is_internal(url)
+#                 if is_internal:
+#                     self.add(link, links, self.exclude)
+#                     self.add(link, self.found_links, self.exclude)
+#         for link in links:
+#             if link not in self.visited_links:
+#                 link = self.normalize(link)
+#                 self.visited_links.append(link)
+#                 self.find(urljoin(self.url,link))
+#
+#
+#     def start(self):
+#         self.find(self.url)
+#         return self.found_links
+#
+#
+#     def add(self, link,link_list,eclude_pattern = None ):
+#         link = self.normalize(link)
+#         if link:
+#             notinlist = link not in link_list
+#             exclude = False
+#             if eclude_pattern :
+#                 exclude = re.search(eclude_pattern,link)
+#             if notinlist and not exclude:
+#                 link_list.append(link)
 
 
 
